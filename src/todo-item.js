ebf33cd9daa246c423aa1023f2b81eb40ff7a67e
zinvergo93 <zachary.coding@gmail.com>
2020-11-03 14:28:10 -0600

    toggle functionality

--- src/todo-item.js
+++ src/todo-item.js
@@ -9,9 +9,15 @@ class TodoItem extends Component {
   }
 
   toggleDone = () => {
-    this.setState((prevState) => ({
-      done: !prevState.done,
-    }));
+    fetch(`http://localhost:5000/api/edit-todo/${this.props.todo.id}`, {
+      method: "PATCH",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ done: !this.state.done }),
+    }).then(() => {
+      this.setState((prevState) => ({
+        done: !prevState.done,
+      }));
+    });
   };
   render() {
     return (
@@ -21,7 +27,7 @@ class TodoItem extends Component {
           checked={this.state.done}
           onChange={this.toggleDone}
         />
-        <h3>{this.props.todo.title}</h3>
+        <p className={this.state.done ? "done" : ""}>{this.props.todo.title}</p>
       </div>
     );
   }
