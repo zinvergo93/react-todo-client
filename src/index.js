9233cffde48d2a6293af1a2d672d2436444180f3
zinvergo93 <zachary.coding@gmail.com>
2020-11-03 11:16:06 -0600

    added style sheet

--- src/index.js
+++ src/index.js
@@ -1,43 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todo: ""
-    }
+      todo: "",
+    };
   }
 
   handleChange = (e) => {
     this.setState({
-      todo: e.target.value
-    })
-  }
+      todo: e.target.value,
+    });
+  };
 
   addTodo = (e) => {
-    console.log("add-todo", this.state.todo)
-    e.preventDefault()
-  }
-  render() { 
-    return ( 
-      <div>
+    console.log("add-todo", this.state.todo);
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <div className="app">
+        <h1>TODO List</h1>
         <form className="add-todo" onSubmit={this.addTodo}>
-          <input 
-          type="text" 
-          placeholder="Add Todo" 
-          value= {this.state.todo} 
-          onChange={this.handleChange}  
+          <input
+            type="text"
+            placeholder="Add Todo"
+            value={this.state.todo}
+            onChange={this.handleChange}
           />
           <button type="submit">Add</button>
-        </form>  
+        </form>
       </div>
-     );
+    );
   }
 }
- 
+
 export default App;
 
-
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
