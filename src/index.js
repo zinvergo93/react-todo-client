408a915dadee3f76f3ab2ca18485a5f2e497d771
zinvergo93 <zachary.coding@gmail.com>
2020-11-03 11:08:20 -0600

    added form for add todo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      todo: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      todo: e.target.value
+    })
+  }
+
+  addTodo = (e) => {
+    console.log("add-todo", this.state.todo)
+    e.preventDefault()
   }
   render() { 
     return ( 
       <div>
-        <h1>TODO List </h1>
-        </div>
+        <form className="add-todo" onSubmit={this.addTodo}>
+          <input 
+          type="text" 
+          placeholder="Add Todo" 
+          value= {this.state.todo} 
+          onChange={this.handleChange}  
+          />
+          <button type="submit">Add</button>
+        </form>  
+      </div>
      );
   }
 }
